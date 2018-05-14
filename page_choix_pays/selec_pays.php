38fe965c85ddf27e7a784778124500162ce3177d
dartek <samsonflorian60410@gmail.com>
2018-05-14 16:47:40 +0200

    Ajout du dossier page_choix_payspage_choix_pays

---
+++ page_choix_pays/selec_pays.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="Styles.css">
+	<title>selection pays</title>
+</head>
+<body>
+	<header>
+    	<div class="Titre">
+         	<div><h1 class="titre1">Ucape - Section</h1></div>
+         	<div>
+         		<p style="color: white">Vous êtes connecté en tant que :</p>
+         		<a  href="#" class="deco" style="color: white">Déconnexion</a>
+         	</div>
+
+        </div>
+	</header>
+
+	<div>
+		<h2 class="Choix">Résultats  :</h2>
+        <a href="#" class="button1">Choix des pays</a>
+        <a href="#" class="button2">Student Profile</a>
+        <a href="#" class="button3">Documentation</a>
+	</div>
+</body>
+</html>
